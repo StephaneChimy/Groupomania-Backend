b8f4f6b400df84aa1321cbbf6ae05f8112feb58a
Stéphane C <64702938+StephaneChimy@users.noreply.github.com>
2021-03-31 12:33:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user");
 const messageRoutes = require("./routes/message");
-//const cors = require('cors');
+const cors = require('cors');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 
@@ -12,20 +12,7 @@ const app = express();
 
 app.use(helmet());
 //app.use(cors({origin:["https://sc-groupomania.netlify.app", "http://localhost:3001"] credentials: true}));
-// Add CORS in the headers
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://sc-groupomania.netlify.app");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
