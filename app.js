de69a53c8d6cc88a46c5fecacb695a12127d6b32
Stéphane Chimy <stephane.akira@gmail.com>
2021-04-02 09:43:43 +0200

    fixed bodyParser deprecated

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(helmet());
 app.use(cors({origin:["https://sc-groupomania.netlify.app", "http://localhost:3001"], credentials: true}));
 //app.use(cors());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(xss());
 // Prevent DOS attacks
 app.use(express.json({ limit: '10kb' })); // Body limit is 10kb
